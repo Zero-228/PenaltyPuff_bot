1c9ef8fb2ef526ff6f9344c8d03dec4a67964790
Zero <nikshkilov@yahoo.com>
2024-05-04 22:26:04 +0200

    Debuging notifications

---
+++ cron/notifications.php
@@ -0,0 +1,33 @@
+<?php 
+
+require '../config.php';
+require '../bot.php';
+
+function sendNotifications(Nutgram $bot) {
+    $dbCon = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    $query = "SELECT * FROM user WHERE lastVisit < NOW() - INTERVAL 1.5 DAY AND deleted = 'no' AND banned = 'no'
+              UNION
+              SELECT puff.userTo, user.language FROM puff JOIN user ON puff.userTo = user.userId WHERE prescribed_at < NOW() - INTERVAL 6 HOUR AND status='pending'";
+    $result = mysqli_query($dbCon, $query);
+    processResults($dbCon, $bot, $result);
+    mysqli_close($dbCon);
+}
+
+function processResults($dbCon, Nutgram $bot, $result) {
+    while ($user = mysqli_fetch_assoc($result)) {
+        $userId = $user['userId'];
+        $logQuery = "SELECT * FROM log WHERE entityId = {$userId} AND context = 'notification'";
+        $logResult = mysqli_query($dbCon, $logQuery);
+        if (mysqli_num_rows($logResult) == 0) {
+            $language = isset($user['language']) ? $user['language'] : 'en';
+            $bot->sendMessage(msg('notification', $language), chat_id: $userId);
+            createLog(TIME_NOW, 'bot', $userId, 'notification', 'notification');
+            sleep(1);
+        }
+    }
+}
+
+// Вызовите функцию sendNotifications
+sendNotifications($bot);
+
+?>
