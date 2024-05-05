b4a87e206df72be4180b2c79e50a86b91f40b2d4
Zero <nikshkilov@yahoo.com>
2024-05-06 01:12:03 +0200

    Notifying admin about bulk results

--- cron/notifications.php
+++ cron/notifications.php
@@ -12,12 +12,16 @@ function sendNotifications(Nutgram $bot) {
     $dbCon = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
     $inactivity_result = mysqli_query($dbCon, "SELECT * FROM user WHERE lastVisit < NOW() - INTERVAL 1.5 DAY AND deleted = 'no' AND banned = 'no'");
     $unapprooved_result = mysqli_query($dbCon, "SELECT puff.userTo, user.language FROM puff JOIN user ON puff.userTo = user.userId WHERE prescribed_at < NOW() - INTERVAL 6 HOUR AND status='pending'");
-    processResults($dbCon, $bot, $inactivity_result, 'inactivity');
-    processResults($dbCon, $bot, $unapprooved_result, 'unapprooved');
+    $num_inac = processResults($dbCon, $bot, $inactivity_result, 'inactivity');
+    $num_unap = processResults($dbCon, $bot, $unapprooved_result, 'unapprooved');
     mysqli_close($dbCon);
+
+    $msg = "🤖 Bot sent notifications to:\n\n   ".$num_inac." inactive users \n   ".$num_unap." unapprooved";
+    $bot->sendMessage($msg, chat_id: ADMIN_ID);
 }
 
 function processResults($dbCon, Nutgram $bot, $result, $messageType) {
+    $counter = 0;
     while ($user = mysqli_fetch_assoc($result)) {
         $userId = $messageType == 'unapprooved' ? $user['userTo'] : $user['userId'];
         $logQuery = "SELECT * FROM log WHERE entityId = {$userId} AND context = 'notification' AND message='{$messageType}'";
@@ -26,9 +30,11 @@ function processResults($dbCon, Nutgram $bot, $result, $messageType) {
             $language = isset($user['language']) ? $user['language'] : 'en';
             $bot->sendMessage(msg($messageType, $language), chat_id: $userId);
             createLog(TIME_NOW, 'bot', $userId, 'notification', $messageType);
+            $counter++;
             sleep(1);
         }
     }
+    return $counter;
 }
 try {
     sendNotifications($bot);
